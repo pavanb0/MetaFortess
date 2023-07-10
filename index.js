ae7ac00ba26df8c04ab40b63e54d3277cc816c87
pavan <pavanbagwe16@gmail.com>
2023-07-10 20:38:12 +0530

    new home page

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ const app = express();
 const port = 3000;
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./Meta.db')
+const path = require('path')
 // add json middleware
 app.use(express.json());
 // send database to client
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'screens','home.html'))
+})
+
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,'screens','login.html'))
+})
+app.get('/signup',(req,res)=>{
+    res.sendFile(path.join(__dirname,'screens','signup.html'))
+})
+
+app.get('/getstarted',(req,res)=>{
+    res.sendFile(path.join(__dirname,'screens','getstart.html'))
+})
+
 app.get('/api', (req, res) => {
     db.all('SELECT * FROM Meta', (err, rows) => {
         if (err) {
@@ -17,6 +34,12 @@ app.get('/api', (req, res) => {
         }
     })
 });
+
+async function fetchapi(){
+    const res = await fetch('https://zenquotes.io/api/quotes/');
+    return res
+}
+
 
 // add new data to database
 app.post('/api', (req, res) => {
